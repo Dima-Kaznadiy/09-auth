bf49fca0d8ab476b391f2253cbc1db8af9909365
Dima Kaznadiy <dimadanced@gmail.com>
2026-04-17 16:37:13 +0300

    mod-6

--- types/user.ts
+++ types/user.ts
@@ -1,26 +1,32 @@
-import { instance } from './api';
+// import { instance } from './api';
 
-export const login = async (data: {
+// export const login = async (data: {
+//     email: string;
+//     password: string;
+// }) => {
+//     const res = await instance.post('/auth/login', data);
+//     return res.data;
+// };
+
+// export const register = async (data: {
+//     email: string;
+//     password: string;
+// }) => {
+//     const res = await instance.post('/auth/register', data);
+//     return res.data;
+// };
+
+// export const logout = async () => {
+//     await instance.post('/auth/logout');
+// };
+
+// export const checkSession = async () => {
+//     const res = await instance.get('/auth/session');
+//     return res.data;
+// };
+
+export interface User {
     email: string;
-    password: string;
-}) => {
-    const res = await instance.post('/auth/login', data);
-    return res.data;
-};
-
-export const register = async (data: {
-    email: string;
-    password: string;
-}) => {
-    const res = await instance.post('/auth/register', data);
-    return res.data;
-};
-
-export const logout = async () => {
-    await instance.post('/auth/logout');
-};
-
-export const checkSession = async () => {
-    const res = await instance.get('/auth/session');
-    return res.data;
-};
+    username: string;
+    avatar: string;
+}
